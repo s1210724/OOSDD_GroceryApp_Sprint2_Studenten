d7f6d55520c85b73df1253a2688e672f4880dedd
s1210724 <s1210724@student.windesheim.nl>
2025-09-18 13:46:19 +0200

    Completed UC06 login feature fully implemented and manually checked.

--- Grocery.Core.Data/Repositories/ClientRepository.cs
+++ Grocery.Core.Data/Repositories/ClientRepository.cs
@@ -19,7 +19,8 @@ namespace Grocery.Core.Data.Repositories
 
         public Client? Get(string email)
         {
-            return clientList[0];
+
+            return clientList.Find(client => client.EmailAddress == email);
         }
 
         public Client? Get(int id)
